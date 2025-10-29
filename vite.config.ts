f4fe020883a45c857043bcb0e6ba09e86e5149f6
Hamas Shafiq <theaudiophile@outlook.com>
2025-10-29 19:25:23 +0000

    Fix homepage URL and base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/snapchat-chats-json-viewer/",
+  base: "/snapchat-json-chat-viewer/",
 });
